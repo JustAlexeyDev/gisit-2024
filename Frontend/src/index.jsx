22f5b7c1068f610b69e4e20df8a2043b6d8e8f17
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-04-05 21:27:31 +0900

    Update index.jsx

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import './Assets/Css/Global.css';
+import './Assets/Css/Reset.css';
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
